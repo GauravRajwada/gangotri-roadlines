311a828c0f78a571ed115cb996aea6942de2d845
GauravRajwada <sintg1999@gmail.com>
2024-08-24 22:59:39 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # gangotri-roadlines
+
+
+
+1. [Truck Image](https://images.unsplash.com/photo-1571088540390-4f8c04afe287?ixid=MnwyMjA3MCx8MHwxfGFsbHwxfHx8fHx8fDE2Mzc3NzU2ODI&ixlib=rb-1.2.1&w=1000&q=80)
+2. [Gangotri Logo](https://gangotri.odoo.com/web/image/website/1/logo?unique=52bb734)
+
+### Icons
+1. [Phone Icon](https://www.flaticon.com/free-icon/phone_106480)
+2. [Building Icon](https://www.flaticon.com/free-icon/building_2990605)
+3. [Sitemap Icon](https://www.flaticon.com/free-icon/sitemap_173637)
+4. [Twitter Icon](https://www.flaticon.com/free-icon/twitter_733579)
+5. [LinkedIn Icon](https://www.flaticon.com/free-icon/linkedin_3536505)
+6. [Instagram Icon](https://www.flaticon.com/free-icon/instagram_2111463)
+
+## Notes
+
+1. All image assets should be stored in the `assets/images` directory.
+2. All icon assets should be stored in the `assets/icons` directory.
+3. Make sure to properly attribute all images and icons if necessary.
+
+Feel free to modify this base according to your requirements. 
+ ```
---
+++ js/main.js
@@ -0,0 +1,119 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+    // Initiate the wowjs with faster animations
+    new WOW({ 
+        animateClass: 'animated',
+        offset:       100,
+        mobile:       true,
+        live:         true 
+    }).init();
+
+    // Add CSS to change animation-duration (for WOW.js animations)
+    $('<style>')
+      .prop('type', 'text/css')
+      .html("\
+      .animated { animation-duration: 0.5s !important; }\
+      ")
+      .appendTo('head');
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo'); // Reduced duration from 1500ms to 1000ms
+        return false;
+    });
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,   // Reduce delay to make it faster
+        time: 1000  // Reduce time to make it faster
+    });
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,  // Reduced transition speed
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 750,  // Reduced transition speed
+        center: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+})(jQuery);
